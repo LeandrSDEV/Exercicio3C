0b01488ecb891de73f327701b31d46d6b5ebcbd6
LeandrSDEV <133284910+LeandrSDEV@users.noreply.github.com>
2024-03-06 07:17:08 -0300

    Add files via upload

---
+++ Fixação Ex3/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Fixação_Ex3.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PedingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
